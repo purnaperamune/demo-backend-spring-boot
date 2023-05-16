3bc20f573b16fb478804a854d8174082a1b14104
Purna Peramune <purnaperamune@gmail.com>
2023-05-16 17:42:37 +0530

    Created two simple endpoints (POST/GET)

---
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
+import org.jvnet.hk2.annotations.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+public class PersonService {
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person){
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople(){
+        return personDao.selectAllPeople();
+    }
+}
