c5dcfa34405b07365ed9571d7fef549f30c09063
Purna Peramune <purnaperamune@gmail.com>
2023-05-16 18:46:36 +0530

    Implemented two more endpoints to update and delete person

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -2,10 +2,13 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("api/v1/person")
 @RestController
@@ -18,7 +21,7 @@ public class PersonController {
     }
 
     @PostMapping
-    public void addPerson(@RequestBody Person person){
+    public void addPerson(@Valid @NonNull @RequestBody Person person){
         personService.addPerson(person);
     }
 
@@ -27,5 +30,18 @@ public class PersonController {
         return personService.getAllPeople();
     }
 
+    @GetMapping(path =  "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id){
+        return personService.getPersonById(id).orElse(null);
+    }
 
+    @DeleteMapping(path =  "{id}")
+    public void deletePersonById(@PathVariable("id") UUID id){
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path =  "{id}")
+    public void updatePerson(@PathVariable("id") UUID id,@Valid @NonNull @RequestBody Person personToUpdate){
+        personService.updatePerson(id, personToUpdate);
+    }
 }
--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class PersonService {
@@ -24,5 +26,17 @@ public class PersonService {
 
     public List<Person> getAllPeople(){
         return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id){
+        return  personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id){
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person newPerson){
+        return personDao.updatePersonById(id, newPerson);
     }
 }
